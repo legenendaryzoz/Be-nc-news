807297388af6778ffbd01e0cc7d84417acedd4fc
Zeyad Gamaleldin <Zeyad.gamaleldin@student.manchester.ac.uk>
2024-08-29 14:08:18 +0100

    Implemented Functionalities to deleteCommentsByID

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const { getArticles } = require('./endpoints/controllers/article.controller');
 const { getCommentsByArticleId } = require('./endpoints/controllers/comments.controller');
 const{postComment} = require('./endpoints/controllers/postcomments.controller');
 const { patchArticleVotes } = require('./endpoints/controllers/patcharticles.controller');
+const {deleteCommentById} = require('./endpoints/controllers/deletecomment.controller');
 const app = express();
 
 app.use(express.json());
@@ -17,6 +18,7 @@ app.get('/api/articles', getArticles);
 app.get('/api/articles/:article_id/comments', getCommentsByArticleId);
 app.post('/api/articles/:article_id/comments', postComment);
 app.patch('/api/articles/:article_id', patchArticleVotes);
+app.delete('/api/comments/:comment_id', deleteCommentById);
 
 app.use((req, res, next) => {
     res.status(404).json({ message: 'Endpoint not found' });
@@ -26,7 +28,11 @@ app.use((req, res, next) => {
     if (err.status) {
         res.status(err.status).send({ msg: err.msg });
     } else if (err.code === '22P02') {
-        res.status(400).send({ msg: 'Invalid article ID' });
+        if (req.originalUrl.includes('/comments/')) {
+            res.status(400).send({ msg: 'Invalid comment ID' });
+        } else {
+            res.status(400).send({ msg: 'Invalid article ID' });
+        }
     } else {
         res.status(500).send({ msg: 'Internal Server Error' });
     }
