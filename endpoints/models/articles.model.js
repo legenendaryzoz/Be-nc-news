4b789ccad9119b0a964670bebbff3cb0a0716251
Zeyad Gamaleldin <Zeyad.gamaleldin@student.manchester.ac.uk>
2024-08-30 12:17:07 +0100

    Implemented functinalities for comment_count

--- endpoints/models/articles.model.js
+++ endpoints/models/articles.model.js
@@ -1,11 +1,19 @@
 const db = require('../../db/connection');
 
 exports.fetchArticleById = (article_id) => {
-    return db.query('SELECT * FROM articles WHERE article_id = $1;', [article_id])
+    return db.query(`
+        SELECT articles.*, COUNT(comments.article_id) AS comment_count
+        FROM articles
+        LEFT JOIN comments ON articles.article_id = comments.article_id
+        WHERE articles.article_id = $1
+        GROUP BY articles.article_id;
+    `, [article_id])
         .then((result) => {
             if (result.rows.length === 0) {
                 return Promise.reject({ status: 404, msg: 'Article not found' });
             }
-            return result.rows[0];
+            const article = result.rows[0];
+            article.comment_count = Number(article.comment_count);
+            return article;
         });
 };
